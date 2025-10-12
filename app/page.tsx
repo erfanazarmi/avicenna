da6942746c78eb9b8bac7cad2f826bdd4b135de9
Erfan <erfankobayashi@gmail.com>
2025-10-12 20:10:59 +0330

    feat: update header with new full-width image

--- app/page.tsx
+++ app/page.tsx
@@ -11,12 +11,7 @@ export default function Home() {
     <>
       <Menu />
       <Header />
-      <main className="
-        flex flex-col items-center gap-30 px-10 py-20
-        bg-[radial-gradient(circle,rgba(0,0,0,0.3)_2px,transparent_2px)]
-        dark:bg-[radial-gradient(circle,rgba(255,255,255,0.2)_2px,transparent_2px)]
-        bg-[length:120px_120px]
-      ">
+      <main className="flex flex-col items-center gap-30 px-10 py-20">
         <Overview />
         <Organizers />
         <Speakers />
--- app/ui/header.tsx
+++ app/ui/header.tsx
@@ -2,53 +2,31 @@ import Image from "next/image";
 
 export default function Header() {
   return (
-    <header className="flex flex-col xl:flex-row items-center justify-center xl:gap-10 p-12 lg:pt-26 bg-navy">
-      <div className="flex flex-col lg:flex-row items-center justify-center gap-10">
-        <div>
-          <Image
-            src="/hero-image.png"
-            alt=""
-            width={480}
-            height={480}
-          />
+    <header className="flex flex-col items-center">
+      <div className="flex flex-col justify-end items-center h-dvh w-full text-center">
+        <div className="relative w-full flex-1 bg-[url('/header.png')] bg-no-repeat bg-cover bg-center bg-transparent">
+          <div className="absolute bottom-0 left-0 w-full h-32 sm:h-60 bg-gradient-to-b from-transparent to-background"></div>
         </div>
-
-        <div className="text-center lg:text-left">
-          <h1 className="font-cinzel leading-none text-6xl font-medium text-gold my-3">
+        <div className="transition -translate-y-10 space-y-6 px-10">
+          <h1 className="text-center font-cinzel text-5xl sm:text-6xl font-medium leading-none text-[#b88f4a] dark:text-[#f2c464]">
             Avicenna
             <br />
             Academy
           </h1>
-          <p className="text-xl text-gray-300 lg:max-w-[500px]">
-            A <strong>computer science</strong> and <strong>tech</strong> academy{" "}
-            <br className="hidden lg:block" />
-            for Persian-speaking learners based in Toronto
+          <p className="text-lg sm:text-xl">
+            A computer science and tech academy for Persian-speaking learners based in Toronto
           </p>
-          <div className="flex flex-row items-center justify-center mt-8">
-            <Image
-              src="/birds.png"
-              alt=""
-              width={130}
-              height={130}
-              className="hidden lg:block mr-2"
-            />
-            <button className="cursor-pointer px-4 py-2 rounded-xl text-lg font-medium text-navy bg-gold">
-              Join the Bootcamp
-            </button>
-          </div>
+          <button className="cursor-pointer px-4 py-2 rounded-xl sm:text-lg font-medium text-white dark:text-black bg-neutral-800 dark:bg-neutral-100">
+            Join the Bootcamp
+          </button>
         </div>
       </div>
-
-      <div className="lg:hidden xl:block w-[1px] h-[60px] xl:h-[320px] my-8 xl:my-0 xl:mx-10 bg-gray-500"></div>
-      <div className="hidden lg:block xl:hidden w-full h-[1px] m-16 bg-gray-700"></div>
-
-      <div>
+      <div className="flex items-center mt-10 px-10">
         <Image
-          src="/trinity-college-toronto-logo.png"
-          alt="Trinity College, University of Toronto"
-          width={200}
-          height={200}
-          className="lg:w-full lg:max-w-[220px]"
+          src="/tree-3.png"
+          alt=""
+          width={240}
+          height={240}
         />
       </div>
     </header>
