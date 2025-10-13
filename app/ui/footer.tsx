9f8774f0979a8eebd03701b145b1c00962b17df8
Erfan <erfankobayashi@gmail.com>
2025-10-13 19:19:29 +0330

    style: update footer UI

--- app/ui/footer.tsx
+++ app/ui/footer.tsx
@@ -4,23 +4,26 @@ export default function Footer() {
   return (
     <>
       <div className="h-8 w-full bg-[url('/border-2.png')] bg-repeat bg-contain"></div>
-      <footer className="flex flex-col justify-center items-center p-8 text-sm text-gold bg-navy">
+      <footer className="flex justify-center items-center gap-2 p-8 text-sm text-gold bg-neutral-800 dark:bg-neutral-900">
         <div>
           <Image
-            src="/square.png"
+            src="/bird.png"
             alt=""
-            width={160}
-            height={160}
+            width={40}
+            height={40}
           />
         </div>
-        <div>Designed & Developed by</div>
-        <a
-          href="https://www.linkedin.com/in/erfanazarmi"
-          target="_blank"
-          className="underline underline-offset-3"
-        >
-          Erfan Azarmi
-        </a>
+        <div>
+          Designed & Developed by
+          <br className="block sm:hidden"/>{" "}
+          <a
+            href="https://www.linkedin.com/in/erfanazarmi"
+            target="_blank"
+            className="underline underline-offset-3"
+          >
+            Erfan Azarmi
+          </a>
+        </div>
       </footer>
     </>
   );
