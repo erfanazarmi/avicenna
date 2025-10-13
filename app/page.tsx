143eec4264171e624abf0bc5be64275b92023c81
Erfan <erfankobayashi@gmail.com>
2025-10-13 18:35:33 +0330

    style: update location section UI

--- app/page.tsx
+++ app/page.tsx
@@ -15,8 +15,8 @@ export default function Home() {
         <Overview />
         <Organizers />
         <Speakers />
-        <Location />
       </main>
+      <Location />
       <Footer />
     </>
   );
--- app/ui/location.tsx
+++ app/ui/location.tsx
@@ -1,51 +1,39 @@
 import Image from "next/image";
+import { SectionTitle } from "@/app/ui/section-title";
 
 export default function Location() {
   return (
-    <section className="flex flex-col items-center justify-center gap-20 w-full">
-      <div className="w-full max-w-[600px] flex flex-col">
-        <Image
-          src="/cloud-1.png"
-          alt=""
-          width={200}
-          height={200}
-          className="self-center"
-        />
-        <Image
-          src="/cloud-2.png"
-          alt=""
-          width={100}
-          height={100}
-          className="self-start"
-        />
-        <Image
-          src="/cloud-3.png"
-          alt=""
-          width={200}
-          height={200}
-          className="self-end"
-        />
-      </div>
+    <section className="flex flex-col items-center justify-center gap-10 p-3 sm:px-20 pb-10 sm:pb-20 w-full">
+      <Image
+        src="/flag.png"
+        alt=""
+        width={200}
+        height={200}
+      />
 
-      <div className="w-full max-w-[1000px] h-140 flex items-center justify-center rounded-xl bg-[url('/trinity-college-toronto.jpg')] bg-cover bg-no-repeat bg-center">
-        <div className="w-full h-full flex flex-col items-center justify-center gap-6 p-10 pt-40 bg-black/10 rounded-xl">
-          <p className="text-center text-xl font-bold uppercase tracking-wide text-white text-shadow-xs text-shadow-black">
-            We look forward to seeing you at {" "}
-            <br className="hidden md:block" />
-            Trinity College, Toronto!
-          </p>
-          <a className="cursor-pointer px-4 py-2 font-bold text-gray-200 bg-navy rounded-xl">
-            Reserve Your Spot
+      <SectionTitle text="location" />
+
+      <div
+        className="
+          flex items-start justify-center w-full h-[80vh] rounded-md
+          bg-[url('/trinity-college-toronto.jpg')] bg-cover bg-no-repeat bg-center
+        "
+      >
+        <div className="flex flex-col items-center justify-center gap-10 mt-20">
+          <div className="bg-white rounded-full w-42 sm:w-54 aspect-square flex items-center justify-center">
+            <Image
+              src="/trinity-college-toronto-logo.png"
+              alt=""
+              width={200}
+              height={200}
+              className="p-5"
+            />
+          </div>
+          <a className="cursor-pointer px-4 sm:px-6 py-2 sm:py-3 w-full text-center text-base sm:text-lg font-medium text-gray-200 bg-navy rounded-xl">
+            Reserve your spot
           </a>
         </div>
       </div>
-
-      <Image
-        src="/bird.png"
-        alt=""
-        width={80}
-        height={80}
-      />
     </section>
   );
 }
